f770246a85f137a1ac27086c613f7fed4350e4af
shakilhossaindeveloper <shakilhossain.developer@gmail.com>
2024-07-12 07:40:36 +0600

    last update

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+Vite Configaration File:
+
+Import { Resolve } From "Path";
+Import { DefineConfig } From "Vite";
+
+Export Default DefineConfig({
+Build: {
+RollupOptions: {
+Input: {
+Main: Resolve(__dirname, "index.html"),
+News: Resolve(__dirname, "news.html"),
+},
+    },
+  },
+});
+
+
